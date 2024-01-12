fe3d334776c2e2d4b6ea5618d6ffa6d85d2d87b6
lovroledic <lovro.ledic03@gmail.com>
2024-01-12 23:00:54 +0100

    Minor tweaks

--- Vjezba4/Vjezba4/Source.c
+++ Vjezba4/Vjezba4/Source.c
@@ -1,12 +1,12 @@
-/* Napisati program za zbrajanje i množenje polinoma. Koeficijenti i eksponenti se
-èitaju iz datoteke.
+/* Napisati program za zbrajanje i množenje polinoma.
+Koeficijenti i eksponenti se čitaju iz datoteke.
 Napomena: Eksponenti u datoteci nisu nužno sortirani. */
 
 #define _CRT_SECURE_NO_WARNINGS
 
-#include<stdio.h>
-#include<stdlib.h>
-#include<string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 #define MAX_LINE 1024
 #define MAX_SIZE 100
@@ -14,7 +14,7 @@ Napomena: Eksponenti u datoteci nisu nužno sortirani. */
 #define EXIT_SUCCESS 0
 #define FILE_ERROR_OPEN -1
 #define MALLOC_ERROR -2
-#define SSCANF_ERROR -3
+#define SCANF_ERROR -3
 
 typedef struct monomial {
 	int coef;
@@ -23,19 +23,19 @@ typedef struct monomial {
 } Mono;
 
 int strToPoly(Mono*, char*);
-int polyAdd(Mono*, Mono*);
+int addMonoToPoly(Mono*, Mono*);
 int printMono(Mono*);
 int printPoly(Mono*);
 int deletePoly(Mono*);
 int polySum(Mono*, Mono*, Mono*);
-int polyMulti(Mono*, Mono*, Mono*);
+int polyProduct(Mono*, Mono*, Mono*);
 
 int main()
 {
-	Mono poly1 = {.coef = 0, .exp = 0, .next = NULL};
-	Mono poly2 = { .coef = 0, .exp = 0, .next = NULL };
-	Mono sum = { .coef = 0, .exp = 0, .next = NULL };
-	Mono multi = { .coef = 0, .exp = 0, .next = NULL };
+	Mono poly1Head = {.coef = 0, .exp = 0, .next = NULL};
+	Mono poly2Head = { .coef = 0, .exp = 0, .next = NULL };
+	Mono sumHead = { .coef = 0, .exp = 0, .next = NULL };
+	Mono productHead = { .coef = 0, .exp = 0, .next = NULL };
 
 
 	int i = 0, size = 0;
@@ -44,58 +44,59 @@ int main()
 	FILE* filePointer = NULL;
 	filePointer = fopen("polynomials.txt", "r");
 
-	if (filePointer == NULL)
-	{
+	if (filePointer == NULL) {
 		printf("File could not be opened.\n");
 		return FILE_ERROR_OPEN;
 	}
 
 	// Reads 1st line and coverts in into poly1
 	fgets(buffer, MAX_LINE, filePointer);
-	strToPoly(&poly1, buffer);
+	strToPoly(&poly1Head, buffer);
 
 	// Reads 2nd line and coverts in into poly2
 	fgets(buffer, MAX_LINE, filePointer);
-	strToPoly(&poly2, buffer);
+	strToPoly(&poly2Head, buffer);
 
-	printPoly(poly1.next);
-	printPoly(poly2.next);
+	printf("Polynomial 1: ");
+	printPoly(poly1Head.next);
+	printf("Polynomial 2: ");
+	printPoly(poly2Head.next);
 
-	polySum(&sum, poly1.next, poly2.next);
-	printPoly(sum.next);
+	polySum(&sumHead, poly1Head.next, poly2Head.next);
+	printf("Sum: ");
+	printPoly(sumHead.next);
 
-	polyMulti(&multi, poly1.next, poly2.next);
-	printPoly(multi.next);
+	polyProduct(&productHead, poly1Head.next, poly2Head.next);
+	printf("Product: ");
+	printPoly(productHead.next);
 
 	
 	fclose(filePointer);
 
-	deletePoly(&poly1);
-	deletePoly(&poly2);
-	deletePoly(&sum);
-	deletePoly(&multi);
+	deletePoly(&poly1Head);
+	deletePoly(&poly2Head);
+	deletePoly(&sumHead);
+	deletePoly(&productHead);
 	
 	return EXIT_SUCCESS;
 }
 
-// Sorted input to polynomial
-int polyAdd(Mono* head, Mono* mono)
+int addMonoToPoly(Mono* head, Mono* mono)
 {
 	Mono* prev = head;
 	Mono* temp = NULL;
 
-	// Monomial with coefficient 0 is equal to 0 so there is no need to add in to polynomial
-	if (mono->coef == 0)
-	{
+	// ax^0 = 0
+	if (mono->coef == 0) {
 		free(mono);
 		return EXIT_SUCCESS;
 	}
 
-	// Find predecessor of first monomial with exponent less than or equal to exponent of new monomial
-	while (prev->next != NULL && prev->next->exp > mono->exp)
+	// Find first monomial with exponent less than or equal to exponent of new monomial
+	while (prev->next != NULL && prev->next->exp > mono->exp) 
 		prev = prev->next;
 
-	// If the next monomial has the same exponent as new monomial, add their coefficients
+	// ax^n + bx^n = (a+b)x^n
 	if (prev->next != NULL && mono->exp == prev->next->exp)
 	{
 		prev->next->coef += mono->coef;
@@ -109,9 +110,8 @@ int polyAdd(Mono* head, Mono* mono)
 		}
 	}
 		
-	// Else, put new monomial after the predecessor
-	else
-	{
+	// Put new monomial after the predecessor
+	else {
 		mono->next = prev->next;
 		prev->next = mono;
 	}
@@ -119,10 +119,9 @@ int polyAdd(Mono* head, Mono* mono)
 	return EXIT_SUCCESS;
 }
 
-// Convert string of a polynomial into a polynomial
 int strToPoly(Mono* head, char* str)
 {
-	int i = 0, size = 0, status = 0;
+	int i = 0, size = 0;
 	Mono* mono = NULL;
 
 	while (strlen(str) > 0)
@@ -130,12 +129,9 @@ int strToPoly(Mono* head, char* str)
 		mono = (Mono*)malloc(sizeof(Mono));
 		if (mono == NULL) return MALLOC_ERROR;
 
-		// %n reads how many characters were read until it was reached
-		status = sscanf(str, " %dx^%d %n", &mono->coef, &mono->exp, &size);
-		if (status != 2) return SSCANF_ERROR; // %n is not included in sscanf return value
+		if (sscanf(str, " %dx^%d %n", &mono->coef, &mono->exp, &size) != 2) return SCANF_ERROR;
 
-		polyAdd(head, mono);
-
+		addMonoToPoly(head, mono);
 		str += size;
 	}
 
@@ -149,16 +145,12 @@ int printMono(Mono* mono)
 	if (mono->exp == 1) printf("x ");
 	else if (mono->exp != 0) printf("x^%d ", mono->exp);
 
-	// If exp == 0, then there won't be an x
-
 	return EXIT_SUCCESS;
 }
 
 int printPoly(Mono* current)
 {
-	// First monomial shouldn't have '+' in front of it
-	if (current != NULL)
-	{
+	if (current != NULL) {
 		printMono(current);
 		current = current->next;
 	}
@@ -182,7 +174,7 @@ int deletePoly(Mono* head)
 	while (head->next != NULL)
 	{
 		temp = head->next;
-		head->next = head->next->next;
+		head->next = temp->next;
 		free(temp);
 	}
 
@@ -192,9 +184,8 @@ int deletePoly(Mono* head)
 int polySum(Mono* sum, Mono* poly1, Mono* poly2)
 {
 	Mono* temp = NULL;
-	temp = (Mono*)malloc(sizeof(Mono));
 
-	while (poly1 != NULL )
+	while (poly1 != NULL)
 	{
 		temp = (Mono*)malloc(sizeof(Mono));
 		if (temp == NULL) return MALLOC_ERROR;
@@ -202,7 +193,7 @@ int polySum(Mono* sum, Mono* poly1, Mono* poly2)
 		temp->coef = poly1->coef;
 		temp->exp = poly1->exp;
 
-		polyAdd(sum, temp);
+		addMonoToPoly(sum, temp);
 		poly1 = poly1->next;
 	}
 
@@ -214,14 +205,14 @@ int polySum(Mono* sum, Mono* poly1, Mono* poly2)
 		temp->coef = poly2->coef;
 		temp->exp = poly2->exp;
 
-		polyAdd(sum, temp);
+		addMonoToPoly(sum, temp);
 		poly2 = poly2->next;
 	}
 
 	return EXIT_SUCCESS;
 }
 
-int polyMulti(Mono* multi, Mono* poly1, Mono* poly2)
+int polyProduct(Mono* product, Mono* poly1, Mono* poly2)
 {
 	Mono* temp = NULL;
 	Mono* poly2first = poly2;
@@ -237,10 +228,9 @@ int polyMulti(Mono* multi, Mono* poly1, Mono* poly2)
 			temp->coef = poly1->coef * poly2->coef;
 			temp->exp = poly1->exp + poly2->exp;
 
-			polyAdd(multi, temp);
+			addMonoToPoly(product, temp);
 			poly2 = poly2->next;
 		}
-
 		poly1 = poly1->next;
 	}
 
